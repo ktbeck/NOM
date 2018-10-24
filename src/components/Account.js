8137ff59852ce335dc6562cec2b137fa5026b3c4
nicolleayon <nicolleayon@gmail.com>
2018-10-23 20:59:26 -0700

    testing out account.js

--- src/components/Account.js
+++ src/components/Account.js
@@ -5,16 +5,20 @@ import { PasswordForgetForm } from './PasswordForget';
 import PasswordChangeForm from './PasswordChange';
 import withAuthorization from './withAuthorization';
 
+
+
 const AccountPage = () =>
   <AuthUserContext.Consumer>
     {authUser =>
       <div>
-        <h1>Account: {authUser.email}</h1>
+        <h1>Welcome: {authUser.fullname}</h1>
+        {/*<h1>Account: {authUser.email}</h1>*/}
         <PasswordForgetForm />
         <PasswordChangeForm />
       </div>
     }
   </AuthUserContext.Consumer>
-  
+
+
 const authCondition = (authUser) => !!authUser;
 export default withAuthorization(authCondition)(AccountPage);
