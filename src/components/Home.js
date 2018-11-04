8ed6a79f9a99822e0d9aa19180bd00bf9cd76d77
Kyler <ktbeck@ucsc.edu>
2018-11-03 21:07:26 -0700

    fixed a few bugs with home

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,89 +5,94 @@ import { db } from '../firebase';
 
 class HomePage extends Component {
   constructor(props) {
-    super(props);
+	super(props);
 
-    this.state = {
-        users: null,
-    };
+	this.state = {
+		users: null,
+	};
   }
 
   componentDidMount() {
-    db.onceGetUsers().then(snapshot =>
-      this.setState({ users: snapshot.val() })
-    );
+	db.onceGetUsers().then(snapshot =>
+	  this.setState({ users: snapshot.val() })
+	);
   }
 
   render() {
 	const { users } = this.state;
 	if(!this.state.users){
-	    return(
-        <div>
-        <h1>Home</h1>
+		return(
+		<div>
+		<h1>Home</h1>
 		No users with meals for sale ):
 		</div>);
-    }
-    const usersList = getUsersWithMeals(users);
-    return (
-      <div>
-        <h1>Home</h1>
-          <div>
-		    <h2> Available Passes</h2>
-            <table style={tableStyle}>
-			    <tbody>
-				    <tr>
-					    <th>Name</th>
-					    <th>Price</th>
-                        <th>Number of Meals</th>
-                    </tr>
-                    {usersList.map((user) =>
-                        <tr>
-                        <td>{user.username}</td>
-                        <td>{user.numMeals}</td>
-                        <td>{user.mealPrice}</td>
-                        <td>
-                            <button onClick={() => buyPass(user.username)}>
-                                Buy Pass
-                            </button>
-                        </td>
-                        </tr>)}
-                </tbody>
-            </table>
-            Average Price: $
-            {avgSellingPrice(users)}
-          </div>
-      </div>
-    );
+	}
+	const usersList = getUsersWithMeals(users);
+	return (
+	  <div>
+		<h1>Home</h1>
+		  <div>
+			<h2> Available Passes</h2>
+			<table style={tableStyle}>
+				<tbody>
+					<tr>
+						<th id="name">Name</th>
+						<th id="price">Price</th>
+						<th id="numMeals">Number of Meals</th>
+					</tr>
+					{usersList.map((user) =>
+						<tr align="center">
+							<td headers="name">{user.username}</td>
+							<td>${user.mealPrice}</td>
+							<td>{user.numMeals}</td>
+							<td>
+								<button onClick={() => buyPass(user.username)}>
+									Buy Pass
+								</button>
+							</td>
+						</tr>)}
+				</tbody>
+			</table>
+			  <div align="center">
+				  <h3>
+					Average Price:
+				  </h3>
+					  ${avgSellingPrice(users)}
+			  </div>
+			</div>
+	  </div>
+	);
   }
 }
 //returns array of all users with at least 1 meal
 function getUsersWithMeals(users){
-    let usersWithMeals = [];
-    for(let i in users){
-        if (parseInt(users[i].numMeals) > 0){
-            usersWithMeals.push(users[i]);
-        }
-    }
-    return usersWithMeals;
+	let usersWithMeals = [];
+	for(let i in users){
+		if (parseInt(users[i].numMeals) > 0 && (parseInt(users[i].mealPrice) > 0)){
+			usersWithMeals.push(users[i]);
+		}
+	}
+	return usersWithMeals;
 }
 //returns average selling price of meals currently
 function avgSellingPrice(users){
-    let sum = 0;
-    let size = 0;
-    for(let i in users) {
-        size++;
-        sum = sum + parseInt(users[i].mealPrice);
-    }
-    console.log(sum);
-    return (sum/size).toFixed(2);
+	let sum = 0;
+	let size = 0;
+	for(let i in users) {
+		size++;
+		sum = sum + parseInt(users[i].mealPrice);
+	}
+	console.log(sum);
+	return (sum/size).toFixed(2);
 }
 
 const tableStyle = {
-  border: '3px solid black'
+  border: '3px solid black',
+  width: '100%',
 };
 //temp function
 function buyPass(user) {
-        console.log("Buying from: " + user);
+		console.log("Buying from: " + user);
 }
 
 const authCondition = (authUser) => !!authUser;
