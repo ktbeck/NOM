a44fad054c0aa46ed5820f3cd09825f54b964a6c
Megan Wu <meganwu313@gmail.com>
2018-11-11 18:14:55 -0800

    added admin page to routes

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -5,4 +5,5 @@ export const HOME = '/home';
 export const ACCOUNT = '/account';
 export const PASSWORD_FORGET = '/pw-forget';
 
-export const VIEW_USER_INFO = '/view-user-info'
+export const VIEW_USER_INFO = '/view-user-info';
+export const ADMIN = '/admin';
