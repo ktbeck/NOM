f7bf681227d3e1d6d345bdf6273206777747cef3
Eric Rong <erong@ucsc.edu>
2018-11-18 15:09:32 -0800

    fix warnings

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as  Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as   Route, Link } from "react-router-dom";
 //import ViewProfile from './ViewProfile';
 
 // import ViewUserInfoButton from './viewUserInfo';
