718aa0240e2e50002ebb32034776d9709b8f0b63
Eric Rong <erong@ucsc.edu>
2018-12-04 21:05:22 -0800

    fixed bug involving db not updating unless initialized

--- src/firebase/db.js
+++ src/firebase/db.js
@@ -6,9 +6,9 @@ export const doCreateUser = (id, username, email) =>
   db.ref(`users/${id}`).set({
     username,
     email,
-    userDescription,
-    paypal,
-    contactinfo
+    userDescription: "",
+    paypal:"",
+    contactinfo:""
   });
 
 export const onceGetUsers = () =>
