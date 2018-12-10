e291190115beeaba5fb85a1ff30c6f233d57875b
Megan Wu <meganwu313@gmail.com>
2018-12-10 01:39:11 -0800

    last build

---
+++ build/precache-manifest.d265a6d44a60dc8ce0be187a50ab8b8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c80a27f8a60a0ebf690d9294abe46bbd",
+    "url": "/static/media/food.c80a27f8.jpeg"
+  },
+  {
+    "revision": "b17ad60722003ae5d2b522b4c1125ead",
+    "url": "/static/media/boy.b17ad607.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a5caddf180726d40e2a4",
+    "url": "/static/js/main.a5caddf1.chunk.js"
+  },
+  {
+    "revision": "cf55f015a66e7a504abc",
+    "url": "/static/js/1.cf55f015.chunk.js"
+  },
+  {
+    "revision": "a5caddf180726d40e2a4",
+    "url": "/static/css/main.5d12fa76.chunk.css"
+  },
+  {
+    "revision": "4818227d13847ff87c52a46c138e7149",
+    "url": "/index.html"
+  }
+];
